f58c2849ac5f9dc3f4be3186ad8228ace82d6d55
naveenluhach <naveenluhach@gmail.com>
2024-01-21 13:08:43 +0530

    added comments to chat room class

--- src/main/java/com/gameof3/gameof3artifact/ChatRoom/ChatRoom.java
+++ src/main/java/com/gameof3/gameof3artifact/ChatRoom/ChatRoom.java
@@ -12,10 +12,28 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 @Builder
 @Document
+/**
+ * Represents a chat room entity in the application.
+ */
 public class ChatRoom {
     @Id
+    /**
+     * Unique identifier for the chat room
+     */
     private String id;
+
+    /**
+     * Identifier for the chat associated with this room
+     */
     private String chatId;
+
+    /**
+     * User ID of the sender in the chat room
+     */
     private String senderId;
+
+    /**
+     * User ID of the recipient in the chat room
+     */
     private String recipientId;
 }
