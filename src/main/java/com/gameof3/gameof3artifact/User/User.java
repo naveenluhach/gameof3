7c49ac2c07ab39d63b2dbdbcc9862afb8347ff4a
naveenluhach <naveenluhach@gmail.com>
2024-01-21 17:39:15 +0530

    added comments to user class

--- src/main/java/com/gameof3/gameof3artifact/User/User.java
+++ src/main/java/com/gameof3/gameof3artifact/User/User.java
@@ -28,32 +28,65 @@ public class User {
      */
     private Status status;
 
+    /**
+     * Getter for 'nickName'.
+     * @return The nickname of the user.
+     */
     public String getNickName() {
         return nickName;
     }
 
+    /**
+     * Setter for 'nickName'.
+     * @param nickName The nickname to set for the user.
+     */
     private void setNickName(String nickName) {
         this.nickName = nickName;
     }
 
+    /**
+     * Getter for 'fullName'.
+     * @return The full name of the user.
+     */
     public String getFullName() {
         return fullName;
     }
 
+    /**
+     * Setter for 'fullName'.
+     * @param fullName The full name to set for the user.
+     */
     private void setFullName(String fullName) {
         this.fullName = fullName;
     }
 
+    /**
+     * Getter for 'status'.
+     * @return The status of the user.
+     */
     public Status getStatus() {
         return status;
     }
 
+    /**
+     * Setter for 'status'.
+     * @param status The status to set for the user.
+     */
     private void setStatus(Status status) {
         this.status = status;
     }
 
-    public User updateStatus(User user, Status status){
+    /**
+     * Static method to update the status of an existing User instance.
+     * @param user The User instance to update.
+     * @param status The new status to set for the user.
+     * @return The updated User instance.
+     */
+    public static User updateStatus(User user, Status status){
+        // Update the status using the setter
         user.setStatus(status);
+
+        // Return the updated User instance
         return user;
     }
 }
